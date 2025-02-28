668e64fd2d6f84f45e59336a301ddac3f74ccb36
JEONTAK <tak980418@naver.com>
2025-02-28 10:17:12 +0900

    first commit

---
+++ src/main/java/com/example/spartadelivery/SpartaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.spartadelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spartadelivery/SpartaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
