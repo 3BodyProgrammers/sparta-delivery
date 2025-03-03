fda7b554f3ae039f6ddc1e30d00d9265175f70d5
jeontak <tak980418@naver.com>
2025-03-03 16:46:08 +0900

    feat(#4) : 메뉴 수정 구현

--- src/main/java/com/example/spartadelivery/domain/menu/controller/MenuController.java
+++ src/main/java/com/example/spartadelivery/domain/menu/controller/MenuController.java
@@ -1,7 +1,9 @@
 package com.example.spartadelivery.domain.menu.controller;
 
 import com.example.spartadelivery.domain.menu.dto.request.MenuSaveRequestDto;
+import com.example.spartadelivery.domain.menu.dto.request.MenuUpdateRequestDto;
 import com.example.spartadelivery.domain.menu.dto.response.MenuSaveResponseDto;
+import com.example.spartadelivery.domain.menu.dto.response.MenuUpdateResponseDto;
 import com.example.spartadelivery.domain.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +17,14 @@ public class MenuController {
     private final MenuService menuService;
 
     @PostMapping("/stores/{storeId}/menus")
-    public ResponseEntity<MenuSaveResponseDto> saveMenus(@PathVariable Long storeId, Long userId, String userRole, @RequestBody MenuSaveRequestDto request){
-        MenuSaveResponseDto response = menuService.saveMenus(storeId, userId, userRole, request);
+    public ResponseEntity<MenuSaveResponseDto> saveMenu(@PathVariable Long storeId, Long userId, String userRole, @RequestBody MenuSaveRequestDto request) {
+        MenuSaveResponseDto response = menuService.saveMenu(storeId, userId, userRole, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/stores/{storeId}/menus/{id}")
+    public ResponseEntity<MenuUpdateResponseDto> updateMenu(@PathVariable Long storeId, @PathVariable Long id, Long userId, String userRole, @RequestBody MenuUpdateRequestDto request) {
+        MenuUpdateResponseDto response = menuService.updateMenu(id, storeId, userId, userRole, request);
         return ResponseEntity.ok(response);
     }
 
---
+++ src/main/java/com/example/spartadelivery/domain/menu/dto/response/MenuUpdateResponseDto.java
@@ -0,0 +1,31 @@
+package com.example.spartadelivery.domain.menu.dto.response;
+
+import com.example.spartadelivery.domain.menu.entity.Menu;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class MenuUpdateResponseDto {
+
+    private final Long id;
+    private final String name;
+    private final Integer price;
+    //TODO : 이후 가게 Response로 교체
+    private final Long storeId;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
+    private MenuUpdateResponseDto(Long id, String name, Integer price, Long storeId, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.storeId = storeId;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public static MenuUpdateResponseDto of(Menu menu) {
+        return new MenuUpdateResponseDto(menu.getId(), menu.getName(), menu.getPrice(), menu.getStoreId(), menu.getCreatedAt(), menu.getModifiedAt());
+    }
+}
