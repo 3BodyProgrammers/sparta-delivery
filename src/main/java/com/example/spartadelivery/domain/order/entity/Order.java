560b95d5eb677d8692ce62b41d546e3c09f2d76d
yrkim <90252861+yoorkim@users.noreply.github.com>
2025-03-05 20:20:19 +0900

    feat(#10) : 리뷰 기능 구현
    * docs(#10) : 리뷰 요구사항 작성
    
    * feat(#10) : 리뷰 기능 구현
    
    * impl(#10) : 리뷰 API 예외처리 수정
    
    * docs(#10) : 리뷰 요구사항 수정
    
    * test(#10) : 리뷰 테스트 구현
    
    * impl(#10) : 리뷰 기능 수정
    
    ---------
    
    Co-authored-by: JEONTAK <tak980418@naver.com>

---
+++ src/main/java/com/example/spartadelivery/domain/order/entity/Order.java
@@ -0,0 +1,54 @@
+package com.example.spartadelivery.domain.order.entity;
+
+import com.example.spartadelivery.common.entity.BaseEntity;
+import com.example.spartadelivery.domain.order.enums.OrderStatus;
+import com.example.spartadelivery.domain.store.entity.Store;
+import com.example.spartadelivery.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(nullable = false)
+    private String menuName;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public static Order toEntity(User user, Store store, String menuName, int price) {
+        Order order = new Order();
+        order.user = user;
+        order.store = store;
+        order.menuName = menuName;
+        order.price = price;
+        order.status = OrderStatus.PENDING;
+        return order;
+    }
+
+    public void updateStatus(OrderStatus newStatus) {
+        this.status = newStatus;
+    }
+
+}
---
+++ src/main/java/com/example/spartadelivery/domain/review/service/ReviewService.java
@@ -0,0 +1,131 @@
+package com.example.spartadelivery.domain.review.service;
+
+import com.example.spartadelivery.common.annotation.Auth;
+import com.example.spartadelivery.common.dto.AuthUser;
+import com.example.spartadelivery.common.exception.CustomException;
+import com.example.spartadelivery.domain.order.entity.Order;
+import com.example.spartadelivery.domain.order.enums.OrderStatus;
+import com.example.spartadelivery.domain.order.service.OrderService;
+import com.example.spartadelivery.domain.review.dto.request.ReviewRequestDto;
+import com.example.spartadelivery.domain.review.dto.response.ReviewPageResponseDto;
+import com.example.spartadelivery.domain.review.dto.response.ReviewResponseDto;
+import com.example.spartadelivery.domain.review.entity.Review;
+import com.example.spartadelivery.domain.review.repository.ReviewRepository;
+import com.example.spartadelivery.domain.store.entity.Store;
+import com.example.spartadelivery.domain.store.service.StoreService;
+import com.example.spartadelivery.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final OrderService orderService;
+    private final StoreService storeService;
+
+    @Transactional
+    public ReviewResponseDto saveReview(@Auth AuthUser authUser, Long orderId, ReviewRequestDto requestDto) {
+        User user = User.fromAuthUser(authUser);
+
+        if (reviewRepository.existsByOrderId(orderId)) {
+            throw new CustomException(HttpStatus.BAD_REQUEST, "이미 작성된 리뷰가 있거나 삭제된 주문입니다. 리뷰를 다시 작성할 수 없습니다.");
+        }
+
+        Order order = orderService.findOrderWithStoreById(orderId);
+
+        if (!isCompleted(order.getStatus())) {
+            throw new CustomException(HttpStatus.FORBIDDEN, "배달이 완료되지 않아 리뷰를 작성할 수 없습니다.");
+        }
+
+        Store store = order.getStore();
+
+        if (store == null || store.getDeletedAt() != null) {
+            throw new CustomException(HttpStatus.NOT_FOUND, "해당 가게가 존재하지 않거나 삭제되었습니다.");
+        }
+
+        Review review = new Review(requestDto.getRating(), requestDto.getComments(), user, store, order);
+        Review savedReview = reviewRepository.save(review);
+
+        return ReviewResponseDto.of(savedReview);
+    }
+
+    @Transactional(readOnly = true)
+    public ReviewPageResponseDto getReviews(Long storeId, int page, int size, Byte minRating, Byte maxRating) {
+        // 가게가 존재 여부 및 삭제 여부 확인
+        storeService.findByIdAndDeletedAtIsNull(storeId);
+
+        // 리뷰 생성일을 기준으로 내림차순 정렬
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.by(Sort.Order.desc("createdAt")));
+
+        Page<Review> reviews = reviewRepository.findAllByStoreIdAndRatingRange(storeId, minRating, maxRating, pageable);
+
+        Long totalReviewCount = reviewRepository.countReviewByStoreId(storeId);
+        Double averageReviewScore = reviewRepository.calculateAverageReviewScore(storeId);
+
+        Page<ReviewResponseDto> reviewResponseDtos = reviews.map(ReviewResponseDto::of);
+
+        return new ReviewPageResponseDto(reviewResponseDtos, totalReviewCount, averageReviewScore);
+    }
+
+    public ReviewResponseDto updateReview(AuthUser authUser, Long id, ReviewRequestDto requestDto) {
+        User user = User.fromAuthUser(authUser);
+
+        Review review = findReviewById(id);
+
+        if (review.getDeletedAt() != null) {
+            throw new CustomException(HttpStatus.NOT_FOUND, "해당 리뷰는 삭제되었습니다.");
+        }
+
+        if (!review.getUser().getId().equals(user.getId())) {
+            throw new CustomException(HttpStatus.FORBIDDEN, "본인이 작성한 리뷰만 수정할 수 있습니다.");
+        }
+
+        review.update(requestDto.getRating(), requestDto.getComments());
+        Review updatedReview = reviewRepository.save(review);
+
+        return ReviewResponseDto.of(updatedReview);
+    }
+
+    @Transactional
+    public String deleteReview(AuthUser authUser, Long id) {
+        User user = User.fromAuthUser(authUser);
+
+        Review review = findReviewById(id);
+
+        if (review.getDeletedAt() != null) {
+            throw new CustomException(HttpStatus.NOT_FOUND, "이미 삭제된 리뷰입니다.");
+        }
+
+        if (!review.getUser().getId().equals(user.getId())) {
+            throw new CustomException(HttpStatus.FORBIDDEN, "본인이 작성한 리뷰만 삭제할 수 있습니다.");
+        }
+
+        review.delete();
+
+        return "리뷰 삭제가 완료되었습니다.";
+    }
+
+    // 배달 완료된 주문인지 확인
+    public boolean isCompleted(OrderStatus status) {
+        return status == OrderStatus.COMPLETED;
+    }
+
+    public Review findReviewById(Long reviewId) {
+        Review review = reviewRepository.findReviewWithStoreById(reviewId)
+                .orElseThrow(() -> new CustomException(HttpStatus.BAD_REQUEST, "리뷰를 찾을 수 없습니다."));
+
+        if (review.getStore() == null || review.getStore().getDeletedAt() != null) {
+            throw new CustomException(HttpStatus.NOT_FOUND, "해당 가게가 존재하지 않거나 삭제되었습니다.");
+        }
+
+        return review;
+    }
+}
